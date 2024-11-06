2ff5c6b03fd271fd8df40ce28cc0351f1f9ab27d
Don Isaac <donald.isaac@gmail.com>
2024-11-06 12:02:10 -0500

    feat: listen for config changes

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,73 @@
 import {
-	commands,
-	ExtensionContext,
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	services,
-	window,
-	workspace,
+    commands,
+    ExtensionContext,
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    services,
+    window,
+    workspace,
 } from 'coc.nvim';
-import {lspName} from './constants';
+import { lspName } from './constants';
 
 export async function activate(context: ExtensionContext) {
-	const config = workspace.getConfiguration('zig');
+    let client: LanguageClient;
+    const config = workspace.getConfiguration('zig');
 
-	if (!config.get('enabled', true)) {
-		return;
-	}
+    let enabled = config.get('enabled', true);
 
-	const command = config.get('path', '');
+    workspace.onDidChangeConfiguration(e => {
+        if (!e.affectsConfiguration('zig')) return;
+        const config = workspace.getConfiguration('zig');
+        const newEnabled = config.get('enabled', true);
+        if (enabled !== newEnabled) {
+            if (newEnabled) {
+                window.showInformationMessage(
+                    `Please reload the window to enable the Zig Language Server.`
+                );
+            } else if (client) {
+                client.stop();
+            }
+        }
+    });
 
-	if (!command) {
-		return window.showErrorMessage(
-			'Failed to find the zls executable! Please specify its path in your settings.',
-		);
-	}
+    if (!enabled) {
+        return;
+    }
 
-	const serverOptions: ServerOptions = {
-		command,
-		args: config.get('debugLog', false) ? ['--debug-log'] : [],
-	};
+    const command = config.get('path', '');
 
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{scheme: 'file', language: 'zig'}],
-		outputChannel: window.createOutputChannel('Zig Language Server'),
-	};
+    if (!command) {
+        return window.showErrorMessage(
+            'Failed to find the zls executable! Please specify its path in your settings.'
+        );
+    }
 
-	const client = new LanguageClient(
-		'zls',
-		lspName,
-		serverOptions,
-		clientOptions,
-	);
+    const serverOptions: ServerOptions = {
+        command,
+        args: config.get('debugLog', false) ? ['--debug-log'] : [],
+    };
 
-	context.subscriptions.push(services.registerLanguageClient(client));
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'zig' }],
+        outputChannel: window.createOutputChannel('Zig Language Server'),
+    };
 
-	if (config.get('startUpMessage', true)) {
-		window.showInformationMessage(`${lspName} running!`);
-	}
+    client = new LanguageClient('zls', lspName, serverOptions, clientOptions);
 
-	commands.registerCommand('zig.start', client.start);
+    context.subscriptions.push(services.registerLanguageClient(client));
 
-	commands.registerCommand('zig.stop', client.stop);
+    if (config.get('startUpMessage', true)) {
+        window.showInformationMessage(`${lspName} running!`);
+    }
 
-	commands.registerCommand('zig.restart', async () => {
-		window.showInformationMessage('Restarting Zig Language Server...');	
-		await client.stop();
-		client.start();
-	});
+    commands.registerCommand('zig.start', client.start);
+
+    commands.registerCommand('zig.stop', client.stop);
+
+    commands.registerCommand('zig.restart', async () => {
+        window.showInformationMessage('Restarting Zig Language Server...');
+        await client.stop();
+        client.start();
+    });
 }
